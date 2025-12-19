438ba7d7622b0c27589c9adae93e10cb1ddbd34e
chenchulakshmi <pattabhichenchu@gmail.com>
2025-12-19 14:45:30 +0530

    msg

---
+++ Day2/RemoveParentheses.java
@@ -0,0 +1,23 @@
+class RemoveParentheses {
+    public String removeOuterParentheses(String s) {
+        int cnt = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                if (cnt > 0) {
+                    sb.append(ch);
+                }
+                cnt++;
+            } else { // ')'
+                cnt--;
+                if (cnt > 0) {
+                    sb.append(ch);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
